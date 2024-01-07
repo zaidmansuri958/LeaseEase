06bd81507b8cfcff09202a1f589b309a6bb20047
zaidmansuri958 <firozam196@gmail.com>
2024-01-07 16:00:57 +0530

    agreement generate

--- src/Pages/Properties.jsx
+++ src/Pages/Properties.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 export const Properties = () => {
   return (
     <div>
-    <Link>
-    <button to='/agreement'>Generate agreement</button>
+    <Link  to='/agreement'>
+    <button>Generate agreement</button>
     </Link>
     </div>
   )
